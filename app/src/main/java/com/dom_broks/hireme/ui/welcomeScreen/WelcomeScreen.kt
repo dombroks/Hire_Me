4bd53e2dab796f04a5e902c67d2700420938e3a5
dombroks <tinoutech@gmail.com>
2020-07-05 21:28:20 +0100

    Building Welcome interface

---
+++ app/src/main/java/com/dom_broks/hireme/ui/welcomeScreen/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.dom_broks.hireme.ui.welcomeScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dom_broks.hireme.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
