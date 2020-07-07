28fc59b47de3567c57df8a24831446b582a750e2
dombroks <tinoutech@gmail.com>
2020-07-07 23:47:17 +0100

    Adding new features

---
+++ app/src/main/java/com/dom_broks/hireme/WhatWeGonnaCallYou.kt
@@ -0,0 +1,11 @@
+package com.dom_broks.hireme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WhatWeGonnaCallYou : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_what_we_gonna_call_you)
+    }
+}
