b70efd4a8bbdf3e2f6cf672ff8fc3a1d9d18ec1e
dombroks <tinoutech@gmail.com>
2020-07-12 15:31:21 +0100

    setting the navigation bottom view

--- app/src/main/java/com/dom_broks/hireme/ui/home/MainActivity.kt
+++ app/src/main/java/com/dom_broks/hireme/ui/home/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dom_broks.hireme.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
 
 /*
         val appBarConfiguration = AppBarConfiguration(
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 */
         val navController = findNavController(R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
-       // setupActionBarWithNavController(navController, appBarConfiguration)
+        // setupActionBarWithNavController(navController, appBarConfiguration)
 
 
     }
