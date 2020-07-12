8ce2e47a15facd791c577028a826b9a38af727e8
dombroks <tinoutech@gmail.com>
2020-07-12 19:26:30 +0100

    Setting the profile fragment

--- app/src/main/java/com/dom_broks/hireme/ProfileFragment.kt
+++ app/src/main/java/com/dom_broks/hireme/ProfileFragment.kt
@@ -1,11 +1,13 @@
 package com.dom_broks.hireme
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.profile_fragment.*
 
 class ProfileFragment : Fragment() {
 
@@ -15,11 +17,13 @@ class ProfileFragment : Fragment() {
 
     private lateinit var viewModel: ProfileViewModel
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.profile_fragment, container, false)
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
