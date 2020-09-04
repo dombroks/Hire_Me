0091d1d749c3a47550e0e3ec55c36a9c3818cdce
dombroks <tinoutech@gmail.com>
2020-09-04 17:08:32 +0100

    AddExperience dialog fragment

--- app/src/main/java/com/dom_broks/hireme/DatePickerDialogFragment.kt
+++ app/src/main/java/com/dom_broks/hireme/DatePickerDialogFragment.kt
@@ -11,8 +11,9 @@ import com.dom_broks.hireme.utils.toMonth
 import kotlinx.android.synthetic.main.date_picker.*
 
 class DatePickerDialogFragment : DialogFragment() {
-    var startingDate : String? = null
-    var endingDate : String? = null
+    var startingDate: String? = null
+    var endingDate: String? = null
+
     companion object {
         const val TAG = "DatePickerDialogFragment"
         fun newInstance(): DatePickerDialogFragment {
@@ -33,10 +34,11 @@ class DatePickerDialogFragment : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setDateBtn.setOnClickListener {
-            startingDate = toMonth(datePicker2.month + 1)+" "+ datePicker2.dayOfMonth + " " + datePicker2.year
+            startingDate =
+                toMonth(datePicker2.month + 1) + " " + datePicker2.dayOfMonth + " " + datePicker2.year
 
-            presentCheckBox.visibility = View.VISIBLE
-            setDateBtn.text = " Set Ending Date "
+          //  presentCheckBox.visibility = View.VISIBLE
+
             Toast.makeText(requireContext(), startingDate, LENGTH_LONG).show()
 
 
