63b120be4e15f8577ce389e461affcfbedded4ff
dombroks <tinoutech@gmail.com>
2020-12-15 11:35:13 +0100

    +-+

---
+++ app/src/main/java/com/dom_broks/hireme/ui/main/subFragment/JobDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.dom_broks.hireme.ui.main.subFragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dom_broks.hireme.R
+
+
+class JobDetailFragment : Fragment(R.layout.fragment_job_detail_fragement) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_job_detail_fragement, container, false)
+    }
+
+    companion object {
+
+        fun newInstance(param1: String, param2: String) =
+            JobDetailFragment()
+    }
+}
